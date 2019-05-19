fba6a6ecd8247d57e6b60dea11cca35858a7894a
AbhisheksoniProjects <abhisheksoni3216@gmail.com>
2019-05-19 21:56:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaseimage4/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.firebaseimage4;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    StorageReference mStorage;
+    Button button;
+    ImageView imageView;
+
+    public static final int GALLERY=10;
+
+    ProgressDialog progressDialog;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        imageView=findViewById(R.id.imageView);
+
+        mStorage= FirebaseStorage.getInstance().getReference();
+
+        progressDialog= new ProgressDialog(this);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+
+                startActivityForResult(intent,GALLERY);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY)
+        {
+            Uri uri= data.getData();
+            progressDialog.setMessage("UPLOADING......");
+            progressDialog.show();
+
+            imageView.setImageURI(uri);
+
+            StorageReference filename=mStorage.child("Photos/" + uri.getLastPathSegment()+".Png" );
+            filename.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(MainActivity.this,"Upload Complete",Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Toast.makeText(MainActivity.this,"Unable to Upload",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+        }
+
+    }
+}
